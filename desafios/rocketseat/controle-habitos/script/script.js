0bd5267a197c0a5b1e01f946ba385e408210ab85
Alexandre Rayol <122651034+alexandrerayol@users.noreply.github.com>
2023-01-21 01:30:30 -0300

    habits project
    Projeto habits finalizado

--- desafios/rocketseat/controle-habitos/script/script.js
+++ desafios/rocketseat/controle-habitos/script/script.js
@@ -1,11 +1,39 @@
 const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
+const nlwSetup = new NLWSetup(form) 
+const button = document.querySelector('button')
 
-const data = {
-    run: ['01-17','01-18','01-19'],
-    water: ['01-17','01-19'],
-    workout: ['01-16','01-18','01-20']
+button.addEventListener('click', addNewDay)
+form.addEventListener('change', saveData)
+
+function addNewDay() {
+
+    
+    const today = new Date().toLocaleDateString('pt-br').slice(0,5)
+    const checkDay = nlwSetup.dayExists(today)  //false or true
+
+    if(checkDay){
+        window.confirm('Dia já existente ❌')
+        return
+    }
+
+    nlwSetup.addDay(today)
+    window.alert('Dia registrado com sucesso ✅')
 }
 
-nlwSetup.setData(data)
+function saveData() {
+
+    const data = nlwSetup.data //Objeto
+    const dataString = JSON.stringify(data) // Objeto -> String
+
+    window.localStorage.setItem('rocketseat@habits', dataString)
+    //reserva os dados no armazenamento local do navegador
+}
+
+const dataString = window.localStorage.getItem('rocketseat@habits') || {}
+
+//Pega os dados armazenados localmente no navegador, caso exista. 
+//Caso não existe, é passado um objeto vazio para evitar erros. 
+
+const dataObject = JSON.parse(dataString) // Converte de string para objeto
+nlwSetup.setData(dataObject) // Atualiza os dados de acordo com o localStorage
 nlwSetup.load()
